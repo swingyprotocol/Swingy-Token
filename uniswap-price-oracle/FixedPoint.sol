6dd1a02e10bcf4d1e055dd78d7361f3d25f1d24e
swingyprotocol <74690424+swingyprotocol@users.noreply.github.com>
2020-11-19 01:47:51 +0000

    Create FixedPoint.sol

---
+++ uniswap-price-oracle/FixedPoint.sol
@@ -0,0 +1,46 @@
+pragma solidity =0.6.6;
+
+library FixedPoint {
+   
+    struct uq112x112 {
+        uint224 _x;
+    }
+   
+    struct uq144x112 {
+        uint _x;
+    }
+
+    uint8 private constant RESOLUTION = 112;
+
+    function encode(uint112 x) internal pure returns (uq112x112 memory) {
+        return uq112x112(uint224(x) << RESOLUTION);
+    }
+
+    function encode144(uint144 x) internal pure returns (uq144x112 memory) {
+        return uq144x112(uint256(x) << RESOLUTION);
+    }
+
+    function div(uq112x112 memory self, uint112 x) internal pure returns (uq112x112 memory) {
+        require(x != 0, 'FixedPoint: DIV_BY_ZERO');
+        return uq112x112(self._x / uint224(x));
+    }
+   
+    function mul(uq112x112 memory self, uint y) internal pure returns (uq144x112 memory) {
+        uint z;
+        require(y == 0 || (z = uint(self._x) * y) / y == uint(self._x), "FixedPoint: MULTIPLICATION_OVERFLOW");
+        return uq144x112(z);
+    }
+
+    function fraction(uint112 numerator, uint112 denominator) internal pure returns (uq112x112 memory) {
+        require(denominator > 0, "FixedPoint: DIV_BY_ZERO");
+        return uq112x112((uint224(numerator) << RESOLUTION) / denominator);
+    }
+
+    function decode(uq112x112 memory self) internal pure returns (uint112) {
+        return uint112(self._x >> RESOLUTION);
+    }
+
+    function decode144(uq144x112 memory self) internal pure returns (uint144) {
+        return uint144(self._x >> RESOLUTION);
+    }
+    }
